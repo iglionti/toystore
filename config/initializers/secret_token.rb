e9be4225ae29acb931f0fdc4bd665be0b47c21a9
Ignacio Lionti <iglionti@iglionti-R430-R480.(none)>
2013-02-06 00:17:29 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toystore::Application.config.secret_token = '0a7a4e940aed2a26c65e51755b29aa7caa6cbba9dddb9b6fa66818a7130fc1a958ee7a357ffaa4ce8c728e3220da60966966d800a8bcd1ccfb480beefe2622bb'
